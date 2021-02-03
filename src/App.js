eedc97758a045c3ef93ac877782ecb02440f6658
Jae Doherty <jaeld97@gmail.com>
2021-02-02 21:16:49 -0800

    changed image sizes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import React from 'react';
 import { Switch, Route } from "react-router-dom";
-import Home from './components/home'
+// import Home from './components/home'
 import Nav from "./components/nav";
 import About from "./components/about";
 import Projects from "./components/projects";
